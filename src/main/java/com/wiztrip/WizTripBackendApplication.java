be6fc8c146427d70007b949504d01b56138665a3
gujaeseong <gjs5448@gmail.com>
2023-11-25 01:57:04 +0900

    프로젝트 생성

---
+++ src/main/java/com/wiztrip/WizTripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wiztrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizTripBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WizTripBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiztrip/WizTripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiztrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizTripBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
