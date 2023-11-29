3d80275b4342ea7f859211d2b03b6ca7df659467
gujaeseong <gjs5448@gmail.com>
2023-11-30 00:50:07 +0900

    Plan, Trip Dto 틀 작성

---
+++ src/main/java/com/wiztrip/dto/PlanDto.java
@@ -0,0 +1,34 @@
+package com.wiztrip.dto;
+
+import lombok.*;
+
+public class PlanDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class PlanPostDto {
+
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class PlanRequestDto {
+
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class PlanPatchDto {
+    }
+
+
+}
---
+++ src/main/java/com/wiztrip/dto/TripDto.java
@@ -0,0 +1,34 @@
+package com.wiztrip.dto;
+
+import lombok.*;
+
+public class TripDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class TripPostDto {
+
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class TripResponseDto {
+
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class TripPatchDto {
+
+    }
+
+}
