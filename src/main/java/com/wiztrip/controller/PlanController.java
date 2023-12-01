cdfa33d9a3647bd0ab66869d2e852fc490394dcf
gujaeseong <gjs5448@gmail.com>
2023-12-01 15:06:17 +0900

    Plan, Trip Controller 작성

--- src/main/java/com/wiztrip/controller/PlanController.java
+++ src/main/java/com/wiztrip/controller/PlanController.java
@@ -1,6 +1,12 @@
 package com.wiztrip.controller;
 
+import com.wiztrip.dto.ListDto;
+import com.wiztrip.dto.PlanDto;
+import com.wiztrip.service.PlanService;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,29 +14,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/trips/{trip_id}/plans")
 public class PlanController {
 
-    @PostMapping
-    public void createPlan(@PathVariable("trip_id") Long tripId) {
+    private final PlanService planService;
 
+    @PostMapping
+    public ResponseEntity<PlanDto.PlanResponseDto> createPlan(@PathVariable("trip_id") @NotNull Long tripId, @RequestBody PlanDto.PlanPostDto planPostDto) {
+        return ResponseEntity.ok().body(planService.createPlan(tripId, planPostDto));
     }
 
     @GetMapping
-    public void getPlan(@PathVariable("trip_id") Long tripId) {
-
+    public ResponseEntity<PlanDto.PlanResponseDto> getPlan(@PathVariable("trip_id") Long tripId, @RequestParam @NotNull @Min(1) Long planId) {
+        return ResponseEntity.ok().body(planService.getPlan(tripId, planId));
     }
 
     @GetMapping(headers = "range=all")
-    public void getAllPlan(@PathVariable("trip_id") Long tripId) {
-
+    public ResponseEntity<ListDto> getAllPlan(@PathVariable("trip_id") Long tripId) {
+        return ResponseEntity.ok().body(planService.getAllPlan(tripId));
     }
 
     @PatchMapping
-    public void updatePlan(@PathVariable("trip_id") Long tripId) {
+    public ResponseEntity<PlanDto.PlanResponseDto> updatePlan(@PathVariable("trip_id") Long tripId, @RequestBody PlanDto.PlanPatchDto planPatchDto) {
+        return ResponseEntity.ok().body(planService.updatePlan(tripId, planPatchDto));
 
     }
 
     @DeleteMapping
-    public void deletePlan(@PathVariable("trip_id") Long tripId) {
-
+    public ResponseEntity<String> deletePlan(@PathVariable("trip_id") Long tripId, @RequestParam @NotNull @Min(1) Long planId) {
+        return ResponseEntity.ok().body(planService.deletePlan(tripId,planId));
     }
 
 }
