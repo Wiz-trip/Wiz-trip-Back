cfb6d07fdfa509121615856fe60759aa523bb503
gujaeseong <gjs5448@gmail.com>
2023-12-06 13:50:29 +0900

    LikeController 수정

--- src/main/java/com/wiztrip/controller/LikeController.java
+++ src/main/java/com/wiztrip/controller/LikeController.java
@@ -1,18 +1,13 @@
 package com.wiztrip.controller;
 
-import com.wiztrip.dto.TripDto;
 import com.wiztrip.service.LikeService;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequiredArgsConstructor
-@NoArgsConstructor
-@RestController("like")
+@RequestMapping("like")
 public class LikeController {
 
      private LikeService likeService;
