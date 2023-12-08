22aaec9a2443c68836710ae5f1eeb3f6037bd1f1
gujaeseong <gjs5448@gmail.com>
2023-12-08 21:24:28 +0900

    Like delete 오류 수정 및 로직 변경

--- src/main/java/com/wiztrip/repository/LandmarkLikeRepository.java
+++ src/main/java/com/wiztrip/repository/LandmarkLikeRepository.java
@@ -1,12 +1,12 @@
 package com.wiztrip.repository;
 
-import com.wiztrip.domain.LandmarkEntity;
 import com.wiztrip.domain.LandmarkLikeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface LandmarkLikeRepository extends JpaRepository<LandmarkLikeEntity,Long> {
+public interface LandmarkLikeRepository extends JpaRepository<LandmarkLikeEntity, Long> {
 
+    boolean existsByLandmarkIdAndLikeId(Long likeId, Long landmarkId);
 }
