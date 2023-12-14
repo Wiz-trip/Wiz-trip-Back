d5fee72e10aa3d6d91b6a38de78a50eb16cf3afc
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-12-15 02:01:28 +0900

    Update UserRepository.java

--- src/main/java/com/wiztrip/repository/UserRepository.java
+++ src/main/java/com/wiztrip/repository/UserRepository.java
@@ -4,11 +4,10 @@ import com.wiztrip.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     UserEntity findByUsername(String username);
 
+    boolean existsByEmail(String email);
 }
