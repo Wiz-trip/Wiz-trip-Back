b3ca53e1512fe9050aae000bf8b8a1eaf5a2a5d3
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-12-15 01:32:57 +0900

    swagger 설명추가(user)
    swagger 설명추가(user)

--- src/main/java/com/wiztrip/service/UserService.java
+++ src/main/java/com/wiztrip/service/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
