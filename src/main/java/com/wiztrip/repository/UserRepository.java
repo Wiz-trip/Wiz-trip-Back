cdc9a6b15f37ae19a6074901a164402e5d27e66f
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-12-15 01:51:26 +0900

    user 수정

--- src/main/java/com/wiztrip/repository/UserRepository.java
+++ src/main/java/com/wiztrip/repository/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     UserEntity findByUsername(String username);
-    Optional<UserEntity> findByEmail(String email);
+    
 }
