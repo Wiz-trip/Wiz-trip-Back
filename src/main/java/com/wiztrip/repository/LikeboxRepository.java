49655af975189135eabd56dfd34884befe176940
gujaeseong <gjs5448@gmail.com>
2023-12-16 05:15:58 +0900

    LikeboxRepository 안쓰는 메소드 삭제

--- src/main/java/com/wiztrip/repository/LikeboxRepository.java
+++ src/main/java/com/wiztrip/repository/LikeboxRepository.java
@@ -10,7 +10,5 @@ import java.util.Optional;
 @Repository
 public interface LikeboxRepository extends JpaRepository<LikeboxEntity,Long> {
 
-    boolean existsByUserId(Long userId);
-
     Optional<LikeboxEntity> findByUserId(Long userId);
 }
