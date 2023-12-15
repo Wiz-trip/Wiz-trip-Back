030ced92f1791f74df21ea8f1a2a81e68242fec5
수현 <su551732@gmail.com>
2023-12-16 04:44:16 +0900

    오류 수정, swagger 설명 추가, 간단한 예외처리 고려

--- src/main/java/com/wiztrip/controller/MemoController.java
+++ src/main/java/com/wiztrip/controller/MemoController.java
@@ -3,37 +3,49 @@ package com.wiztrip.controller;
 import com.wiztrip.dto.ListDto;
 import com.wiztrip.dto.MemoDto;
 import com.wiztrip.service.MemoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Memo")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("trips/{tripId}/memos")
+@RequestMapping("trips/{trip_id}/memos")
 public class MemoController {
 
-    private MemoService memoService;
+    private final MemoService memoService;
 
     // 메모 생성
     @PostMapping
+    @Operation(summary = "Memo 생성",
+            description = "trip_id와 MemoPostDto를 사용해 해당 Trip(전체 여행 계획)에 속한 Memo를 생성합니다.")
     public ResponseEntity<MemoDto.MemoResponseDto> createMemo(@PathVariable("trip_id") Long tripId, @RequestBody MemoDto.MemoPostDto memoPostDto) {
         return ResponseEntity.ok().body(memoService.createMemo(tripId, memoPostDto));
     }
 
     // 메모 조회 (전체 전달)
     @GetMapping
-    public ResponseEntity<ListDto> getMemoAll(@PathVariable("trip_id") Long tripId) {
+    @Operation(summary = "Memo 조회",
+            description = "trip_id를 사용해 카테고리 상관 없이 해당 Trip(전체 여행 계획)에 작성된 모든 Memo를 전달합니다. " +
+                    "만약 아직 작성된 Memo가 없을 경우, NOT_FOUND(404) 예외를 보내도록 처리했습니다.")
+    public ResponseEntity<ListDto<MemoDto.MemoResponseDto>> getMemoAll(@PathVariable("trip_id") Long tripId) {
         return ResponseEntity.ok().body(memoService.getMemoAll(tripId));
     }
 
     // 메모 수정
     @PatchMapping
+    @Operation(summary = "Memo 수정",
+            description = "trip_id와 MemoPatchtDto를 사용해 해당 Memo를 삭제합니다.")
     public ResponseEntity<MemoDto.MemoResponseDto> updateMemo(@PathVariable("trip_id") Long tripId, @RequestBody MemoDto.MemoPatchDto memoPatchDto) {
         return ResponseEntity.ok().body(memoService.updateMemo(tripId, memoPatchDto));
     }
 
     // 메모 삭제
-    @DeleteMapping("/{memoId}")
+    @DeleteMapping("/{memo_id}")
+    @Operation(summary = "Memo 삭제",
+            description = "trip_id와 memo_id를 사용해 해당 Memo를 삭제합니다.")
     public ResponseEntity<String> deleteMemo(@PathVariable("trip_id") Long tripId, @PathVariable("memo_id") Long memoId) {
         return ResponseEntity.ok().body(memoService.deleteMemo(tripId, memoId));
     }
--- src/main/java/com/wiztrip/service/MemoService.java
+++ src/main/java/com/wiztrip/service/MemoService.java
@@ -3,47 +3,68 @@ package com.wiztrip.service;
 import com.wiztrip.domain.MemoEntity;
 import com.wiztrip.dto.ListDto;
 import com.wiztrip.dto.MemoDto;
+import com.wiztrip.exception.CustomException;
+import com.wiztrip.exception.ErrorCode;
 import com.wiztrip.mapstruct.MemoMapper;
 import com.wiztrip.repository.MemoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
 public class MemoService {
 
-    private MemoMapper memoMapper;
+    private final MemoMapper memoMapper;
 
-    private MemoRepository memoRepository;
+    private final MemoRepository memoRepository;
 
+    @Transactional
     public MemoDto.MemoResponseDto createMemo(Long tripId, MemoDto.MemoPostDto memoPostDto) {
         return memoMapper.toResponseDto(memoRepository.save(memoMapper.toEntity(memoPostDto, tripId)));
     }
 
     // 원하는 데이터 담아서 변환해준 후, ListDto를 통해 리스트화 필요
-    public ListDto getMemoAll(Long tripId) {
-        return new ListDto(memoRepository.findAllByTripId(tripId).stream().map(o -> {
+    public ListDto<MemoDto.MemoResponseDto> getMemoAll(Long tripId) {
+        List<MemoEntity> memoList = memoRepository.findAllByTripId(tripId);
+
+        // 작성된 Memo가 없는 경우(null)
+        if (memoList.isEmpty()) {
+            throw new CustomException(ErrorCode.MEMO_NOT_EXIST);
+        }
+
+        return new ListDto<>(memoList.stream().map(o -> {
             checkValid(o, tripId);
             return memoMapper.toResponseDto(o);
         }).toList());
     }
 
+    @Transactional
     public MemoDto.MemoResponseDto updateMemo(Long tripId, MemoDto.MemoPatchDto memoPatchDto) {
-        MemoEntity memo = memoRepository.findById(memoPatchDto.getMemoId()).orElseThrow();
+        MemoEntity memo = memoRepository.findById(memoPatchDto.getMemoId())
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMO_NOT_FOUND));
         checkValid(memo, tripId);
         memoMapper.updateFromPatchDto(memoPatchDto, memo);
-        return memoMapper.toResponseDto(memoRepository.findById(memoPatchDto.getMemoId()).orElseThrow());
+        return memoMapper.toResponseDto(memo);
     }
 
+    @Transactional
     public String deleteMemo(Long tripId, Long memoId) {
-        if (!memoRepository.existsById(memoId)) return "memoId: " + memoId + "  존재하지 않는 memo입니다.";
-        checkValid(memoRepository.findById(memoId).orElseThrow(),tripId);
+        if (!memoRepository.existsById(memoId)) {
+            throw new CustomException(ErrorCode.MEMO_NOT_FOUND);
+        }
+        checkValid(memoRepository.findById(memoId).orElseThrow(), tripId);
         memoRepository.deleteById(memoId);
         return "memoId: " + memoId + " 삭제 완료";
     }
 
     // 해당 trip에 속한 memo인지 확인
     private static void checkValid(MemoEntity memo, Long tripId) {
-        if (!memo.getTrip().getId().equals(tripId)) throw new RuntimeException("Permission denied");
+        if (!memo.getTrip().getId().equals(tripId)) throw new CustomException(ErrorCode.NOT_IN_TRIP_MEMO);
     }
 }
