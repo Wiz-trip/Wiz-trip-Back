00cb41253fe33f6d9470345eb3ebfe03eba57de0
gujaeseong <gjs5448@gmail.com>
2023-12-21 03:27:17 +0900

    Trip checkValid 추가

--- src/main/java/com/wiztrip/controller/TripController.java
+++ src/main/java/com/wiztrip/controller/TripController.java
@@ -26,16 +26,17 @@ public class TripController {
 
     @Operation(summary = "Trip 생성", description = "TripPostDto를 사용해 Trip 생성. TripPostDto.userIdList에 자신의 id 꼭 포함해야함!!!")
     @PostMapping
-    public ResponseEntity<TripDto.TripResponseDto> createTrip(@RequestBody TripDto.TripPostDto tripPostDto) {
-        return ResponseEntity.ok().body(tripService.createTrip(tripPostDto));
+    public ResponseEntity<TripDto.TripResponseDto> createTrip(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody TripDto.TripPostDto tripPostDto) {
+        return ResponseEntity.ok().body(tripService.createTrip(principalDetails.getUser(), tripPostDto));
     }
 
     @Operation(summary = "Trip 조회", description = "trip id를 통해 Trip 정보 조회")
     @GetMapping
     public ResponseEntity<TripDto.TripResponseDto> getTrip(
+            @AuthenticationPrincipal PrincipalDetails principalDetails,
             @Schema(description = "trip id", example = "1")
             @RequestParam @NotNull @Min(1) Long tripId) {
-        return ResponseEntity.ok().body(tripService.getTrip(tripId));
+        return ResponseEntity.ok().body(tripService.getTrip(principalDetails.getUser(),tripId));
     }
 
     @Operation(summary = "User가 속한 Trip id 조회 - Page",description = "User가 속한 Trip의 id를 담은 Page 조회. JWT Token 필수!!!")
@@ -66,16 +67,17 @@ public class TripController {
 
     @Operation(summary = "Trip 수정", description = "TripPatchDto를 통해 Trip 수정. TripPatchDto.userIdList, TripPatchDto.planIdList를 조정해 참여중인 User, 포함된 Plan을 추가, 삭제 할 수 있음. 수정하고싶은 attribute만 포함해서 보내야함!! ")
     @PatchMapping
-    public ResponseEntity<TripDto.TripResponseDto> updateTrip(@RequestBody TripDto.TripPatchDto tripPatchDto) {
-        return ResponseEntity.ok().body(tripService.updateTrip(tripPatchDto));
+    public ResponseEntity<TripDto.TripResponseDto> updateTrip(@AuthenticationPrincipal PrincipalDetails principalDetails,@RequestBody TripDto.TripPatchDto tripPatchDto) {
+        return ResponseEntity.ok().body(tripService.updateTrip(principalDetails.getUser(),tripPatchDto));
     }
 
     @Operation(summary = "Trip 삭제", description = "trip id를 통해 trip 삭제")
     @DeleteMapping
     public ResponseEntity<String> deleteTrip(
+            @AuthenticationPrincipal PrincipalDetails principalDetails,
             @Schema(description = "trip id",example = "1")
             @RequestParam @NotNull @Min(1) Long tripId) {
-        return ResponseEntity.ok().body(tripService.deleteTrip(tripId));
+        return ResponseEntity.ok().body(tripService.deleteTrip(principalDetails.getUser(),tripId));
     }
 
 
--- src/main/java/com/wiztrip/service/TripService.java
+++ src/main/java/com/wiztrip/service/TripService.java
@@ -5,6 +5,7 @@ import com.wiztrip.domain.UserEntity;
 import com.wiztrip.dto.TripDto;
 import com.wiztrip.mapstruct.TripMapper;
 import com.wiztrip.repository.TripRepository;
+import com.wiztrip.repository.TripUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -22,13 +23,19 @@ public class TripService {
 
     private final TripRepository tripRepository;
 
+    private final TripUserRepository tripUserRepository;
+
     @Transactional
-    public TripDto.TripResponseDto createTrip(TripDto.TripPostDto tripPostDto) {
+    public TripDto.TripResponseDto createTrip(UserEntity user, TripDto.TripPostDto tripPostDto) {
+        //본인이 userIdList에 포함되어 있지 않으면 추가
+        if(!tripPostDto.getUserIdList().contains(user.getId())) tripPostDto.getUserIdList().add(user.getId());
         return tripMapper.toResponseDto(tripRepository.save(tripMapper.toEntity(tripPostDto)));
     }
 
-    public TripDto.TripResponseDto getTrip(Long tripId) {
-        return tripMapper.toResponseDto(tripRepository.findById(tripId).orElseThrow());
+    public TripDto.TripResponseDto getTrip(UserEntity user, Long tripId) {
+        TripEntity trip = tripRepository.findById(tripId).orElseThrow();
+        checkValid(user.getId(), trip.getId());
+        return tripMapper.toResponseDto(trip);
     }
 
     public Page<Long> getTripIdPageByUser(UserEntity user, Pageable pageable) {
@@ -40,16 +47,22 @@ public class TripService {
     }
 
     @Transactional
-    public TripDto.TripResponseDto updateTrip(TripDto.TripPatchDto tripPatchDto) {
+    public TripDto.TripResponseDto updateTrip(UserEntity user, TripDto.TripPatchDto tripPatchDto) {
         TripEntity trip = tripRepository.findById(tripPatchDto.getTripId()).orElseThrow();
+        checkValid(user.getId(), trip.getId());
         tripMapper.updateFromPatchDto(tripPatchDto, trip);
         return tripMapper.toResponseDto(tripRepository.findById(tripPatchDto.getTripId()).orElseThrow());
     }
 
     @Transactional
-    public String deleteTrip(Long tripId) {
+    public String deleteTrip(UserEntity user, Long tripId) {
         if (!tripRepository.existsById(tripId)) return "tripId: "+tripId+" 존재하지 않는 trip입니다.";
+        checkValid(user.getId(),tripId);
         tripRepository.deleteById(tripId);
         return "tripId: " + tripId + " 삭제 완료";
+    }
+
+    private void checkValid(Long userId, Long tripId) {
+        if(!tripUserRepository.existsByUserIdAndTripId(userId,tripId)) throw new RuntimeException("Permission denied");
     }
 }
