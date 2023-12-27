dc4e43d5f07bc302fd2b685fe3572eae4c8cd124
수현 <su551732@gmail.com>
2023-12-27 13:51:05 +0900

    swagger 설명 추가

--- src/main/java/com/wiztrip/controller/ReviewController.java
+++ src/main/java/com/wiztrip/controller/ReviewController.java
@@ -3,6 +3,8 @@ package com.wiztrip.controller;
 import com.wiztrip.config.spring_security.auth.PrincipalDetails;
 import com.wiztrip.dto.ReviewDto;
 import com.wiztrip.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Review")
 @RestController
 @RequiredArgsConstructor
 public class ReviewController {
@@ -19,6 +22,8 @@ public class ReviewController {
 
     // 후기글 생성
     @PostMapping("trips/{tripId}/reviews")
+    @Operation(summary = "Review 생성",
+            description = "tripId와 ReviewPostDto를 사용해 해당 Trip(전체 여행 계획)에 속한 Review를 생성합니다.")
     public ResponseEntity<ReviewDto.ReviewResponseDto> createReview(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
             @PathVariable("tripId") Long tripId,
@@ -28,12 +33,20 @@ public class ReviewController {
 
     // 후기글 조회 (세부 사항)
     @GetMapping("trips/{tripId}/reviews/{reviewId}")
+    @Operation(summary = "Review 조회(세부 사항)",
+            description = "tripId와 reviewId를 사용해 선택된 Review를 조회합니다.")
     public ResponseEntity<ReviewDto.ReviewResponseDto> getReview(@PathVariable("tripId") Long tripId, @PathVariable("reviewId") Long reviewId) {
         return ResponseEntity.ok().body(reviewService.getReview(tripId, reviewId));
     }
 
     // 후기글 조회 (전체, 마이페이지)
     @GetMapping("my-reviews")
+    @Operation(summary = "Review 조회(마이 페이지)",
+            description = """
+                마이 페이지에서 해당 User가 작성한 review를 Trip(전체 여행 계획)의 startDate를 기준으로 전달합니다.
+                페이징을 사용해 한 페이지에 몇 개의 Review를 받고 싶은지 설정할 수 있고, 원하는 페이지에 속한 Review를 확인할 수 있습니다.
+                """
+    )
     public ResponseEntity<Page<ReviewDto.MyReviewResponseDto>> getMyReview(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
             @RequestParam(defaultValue = "0") Integer pageNum,
@@ -48,6 +61,8 @@ public class ReviewController {
 
     // 후기글 수정
     @PatchMapping("trips/{tripId}/reviews")
+    @Operation(summary = "Review 수정",
+            description = "tripId와 ReviewPatchDto를 사용해 해당 Review를 삭제합니다.")
     public ResponseEntity<ReviewDto.ReviewResponseDto> updateReview(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
             @PathVariable("tripId") Long tripId,
@@ -57,6 +72,8 @@ public class ReviewController {
 
     // 후기글 삭제
     @DeleteMapping("trips/{tripId}/reviews/{reviewId}")
+    @Operation(summary = "Review 삭제",
+            description = "tripId와 reviewId를 사용해 해당 Review를 삭제합니다.")
     public ResponseEntity<String> deleteReview(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
             @PathVariable("tripId") Long tripId,
